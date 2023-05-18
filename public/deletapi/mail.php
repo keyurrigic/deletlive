0a1211a69214d15f7c24e288cd3688ab015c0a08
keyurrigic <keyur@rigicglobalsolutions.com>
2023-05-18 12:02:02 +0530

    Update Mail Settings

--- public/deletapi/mail.php
+++ public/deletapi/mail.php
@@ -31,6 +31,53 @@ if(empty($name) || empty($email) || empty($address))
     die;
 }
 
+if(empty($applyUrl) || $applyUrl==""){
+    $body='If you would like to apply please follow the link below.<br/>
+    Along with your application please provide the following:<br/>
+    Thank you!';
+}
+else{
+    $body='If you would like to apply please follow the link below.<br/>
+    <a href="'.$applyUrl.'">Click Here</a><br/>
+    Thank you!';
+}
+
+$params = array(
+    'to'        => $email,
+    'toname'    => $name,
+    'from'      => "noreply@delet.com",
+    'fromname'  => "EavlyLA",
+    'subject'   => "Address: ".$address. ' Rental Application',
+    'text'      => "",
+    'html'      => $body,
+  );
+$url = 'https://api.sendgrid.com/';
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer 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'));
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+//print_r($response);
+
+$data = array('success'=>true,"message"=>"Email Sent");
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($data);
+die;
 $mail = new PHPMailer(true);
 
 try {
